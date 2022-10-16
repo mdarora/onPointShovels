a455af8574d7a8ff469e29df728f68f8774cf654
mdarora <mdarora0001@gmail.com>
2022-10-16 15:48:24 -0400

    added validations

--- Models/Shovel.cs
+++ Models/Shovel.cs
@@ -10,19 +10,31 @@ namespace OnPointShovels.Models
     {
         public int Id { get; set; }
 
+        [StringLength(80, MinimumLength = 5)]
+        [Required]
         public string Title { get; set; }   // title of the product
 
+        [StringLength(50, MinimumLength = 5)]
+        [Required]
         public string Type { get; set; }    // type of product
 
+        [StringLength(40, MinimumLength = 2)]
+        [Required]
         public string Color { get; set; }   // color of that product
 
+        [DisplayName("Weight (lbs)")]
         public int Weight { get; set; }     // weight of the product in lbs
 
+        [DisplayName("Height (inch)")]
         public int Height { get; set; }     // height of the product in inces
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }  // price of the product
 
         //adding new field
+        [Range(1, 5)]
         public int Rating { get; set; }  // Rating for the product
     }
 }
