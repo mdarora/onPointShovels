01b5e2a51f19101662fbc8944a0453f2fdb74ca0
mdarora <mdarora0001@gmail.com>
2022-10-16 15:14:37 -0400

    created view model for shovels

---
+++ Models/ShovelTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace OnPointShovels.Models
+{
+    public class ShovelTypeViewModel
+    {   // view model for the type field in shovel model
+        public List<Shovel> Shovels { get; set; }
+        public SelectList Types { get; set; }
+        public string ShovelType { get; set; }
+        public string SearchString { get; set; }
+    }
+}
