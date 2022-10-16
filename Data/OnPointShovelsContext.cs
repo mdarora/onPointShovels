cdbe916c4e40f89aa1e1c9efce16e7c8bcf7700d
mdarora <mdarora0001@gmail.com>
2022-10-16 14:15:41 -0400

    db context of onpoint shovel

---
+++ Data/OnPointShovelsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnPointShovels.Models;
+
+namespace OnPointShovels.Data
+{
+    public class OnPointShovelsContext : DbContext
+    {
+        // Database context class for the company onPoint Shovels
+        public OnPointShovelsContext(DbContextOptions<OnPointShovelsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shovel> Shovel { get; set; }
+    }
+}
